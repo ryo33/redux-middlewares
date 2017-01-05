18d5bf8961f18047d78a05456ee5f18f6e28ec76
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-01-05 16:40:48 +0900

    Modify the filter implementation

--- src/createFilter.js
+++ src/createFilter.js
@@ -2,10 +2,6 @@ const createMiddleware = require('./createMiddleware.js')
 
 module.exports = (...matchers) => {
   const filter = matchers.pop()
-  const callback = ({getState, nextDispatch, action}) => {
-    if (filter({getState, action})) {
-      return nextDispatch(action)
-    }
-  }
-  return createMiddleware(...matchers, callback)
+  const negatedFilter = (arg) => !filter(arg)
+  return createMiddleware(...matchers, negatedFilter, () => {})
 }
